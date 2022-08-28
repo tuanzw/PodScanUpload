86c630d190bfd30dc891287afa373b8be9f83da0
Tuan Nguyen <tuanzw@gmail.com>
2022-08-28 19:41:15 +0700

    add base.html

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
 function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
+    console.log('Code matched = ${decodedText}', decodedResult);
 }
 
 function onScanFailure(error) {
-    console.warn(`Code scan error = ${error}`);
+    console.warn('Code scan error = ${error}');
 }
 
 let html5QrcodeScanner = new Html5QrcodeScanner("reader",{ fps: 10, qrbox: {width: 250, height: 250} },/* verbose= */ false);
